9935ee9f3854e50cec0f7275b28277503ba83d00
Hrvoje Lukšić <hluksic20@foi.hr>
2023-07-27 18:25:30 +0200

    Fixed stoi() crash

--- ElementaryCellularAutomata/console.cpp
+++ ElementaryCellularAutomata/console.cpp
@@ -6,35 +6,6 @@ Console::Console(Automaton* automaton)
 	this->automaton = automaton;
 }
 
-void Console::ParseCommand(olc::PixelGameEngine* pge, const std::string& text)
-{
-	if (IsEmptyOrWhitespace(text))
-	{
-		pge->ConsoleClear();
-		return;
-	}
-
-	auto tokens = Tokenize(text);
-	bool NoArguments = (tokens[0] == tokens[1]) || tokens.size() == 1;
-
-	if (tokens[0] == "help" && NoArguments)
-		PrintHelpText();
-	else if (tokens[0] == "run" && NoArguments)
-		automaton->ClearAndRun();
-	else if (tokens[0] == "getrule" && NoArguments)
-		std::cout << "Current rule: " << automaton->GetRule() << "\n\n";
-	else if (tokens[0] == "clear" && NoArguments)
-		automaton->Clear();
-	else if (tokens[0] == "clearconsole" && NoArguments)
-		pge->ConsoleClear();
-	else if (tokens[0] == "setstate")
-		SetAutomatonState(tokens[1]);
-	else if (tokens[0] == "setrule")
-		SetAutomatonRule(tokens[1]);
-	else
-		std::cout << "Invalid command: '" << text << "'. Type 'help' for a list of commands.\n\n";
-}
-
 static bool IsEmptyOrWhitespace(const std::string& text)
 {
 	return text.empty() || std::all_of(text.begin(), text.end(), isspace);
@@ -46,6 +17,55 @@ static auto Tokenize(const std::string& text)
 	std::istringstream iss(text);
 	std::copy(std::istream_iterator<std::string>(iss), std::istream_iterator<std::string>(), std::back_inserter(tokens));
 	return tokens;
+}
+
+static void PrintHelpText()
+{
+	std::cout << "Available commands:\n";
+	std::cout << "help         - Displays this message.\n";
+	std::cout << "run          - Runs the simulation (shortcut CTRL).\n";
+	std::cout << "setrule      - Sets the automaton [0, 255].\n";
+	std::cout << "getrule      - Prints the current automaton.\n";
+	std::cout << "setstate     - Sets the initial generation state (left, right, center, random).\n";
+	std::cout << "clear        - Clear screen (shortcut C).\n";
+	std::cout << "clearconsole - Clear console history.\n\n";
+}
+
+void Console::ParseCommand(olc::PixelGameEngine* pge, const std::string& text)
+{
+	if (IsEmptyOrWhitespace(text))
+	{
+		pge->ConsoleClear();
+		return;
+	}
+
+	auto tokens = Tokenize(text);
+	bool NoArguments = tokens.size() == 1;
+
+	if (NoArguments)
+	{
+		if (tokens[0] == "help")
+			PrintHelpText();
+		else if (tokens[0] == "run")
+			automaton->ClearAndRun();
+		else if (tokens[0] == "getrule")
+			std::cout << "Current rule: " << automaton->GetRule() << "\n\n";
+		else if (tokens[0] == "clear")
+			automaton->Clear();
+		else if (tokens[0] == "clearconsole")
+			pge->ConsoleClear();
+		else
+			std::cout << "Invalid command: '" << text << "'. Type 'help' for a list of commands.\n\n";
+		
+		return;
+	}
+
+	if (tokens[0] == "setstate")
+		SetAutomatonState(tokens[1]);
+	else if (tokens[0] == "setrule")
+		SetAutomatonRule(tokens[1]);
+	else
+		std::cout << "Invalid command: '" << text << "'. Type 'help' for a list of commands.\n\n";
 }
 
 void Console::SetAutomatonState(const std::string& state)
@@ -64,8 +84,18 @@ void Console::SetAutomatonState(const std::string& state)
 
 void Console::SetAutomatonRule(const std::string& argument)
 {
-	int rule = std::stoi(argument);
-		
+	int rule = 0;
+
+	try
+	{
+		rule = std::stoi(argument);
+	}
+	catch (...)
+	{
+		std::cout << "Invalid rule: '" << argument << "'. Value must be in range [0, 255].\n\n";
+		return;
+	}
+	
 	if (rule < 0 || rule > 255)
 	{
 		std::cout << "Invalid rule: '" << rule << "'. Value must be in range [0, 255].\n\n";
@@ -74,15 +104,3 @@ void Console::SetAutomatonRule(const std::string& argument)
 
 	automaton->SetRule(rule);
 }
-
-static void PrintHelpText()
-{
-	std::cout << "Available commands:\n";
-	std::cout << "help         - Displays this message.\n";
-	std::cout << "run          - Runs the simulation (shortcut CTRL).\n";
-	std::cout << "setrule      - Sets the automaton [0, 255].\n";
-	std::cout << "getrule      - Prints the current automaton.\n";
-	std::cout << "setstate     - Sets the initial generation state (left, right, center, random).\n";
-	std::cout << "clear        - Clear screen (shortcut C).\n";
-	std::cout << "clearconsole - Clear console history.\n\n";
-};
